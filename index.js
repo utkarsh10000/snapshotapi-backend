1f39870900559d98a5bb1717dcbc1d16bed3f0a7
utkarsh <us5927060@gmail.com>
2026-03-22 20:52:33 +0530

    fix cors for production

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ const authRoute = require('./routes/auth')
 const { globalLimiter, screenshotLimiter } = require('./middleware/rateLimiter')
 const session = require('express-session')
 const passport = require('./config/passport')
-const billingRoutes = require('./routes/billing');
+const billingRoutes = require('./routes/billing')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
-app.use('/api/billing/webhook', express.raw({ type: 'application/json' }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'https://snapshotapi-2qj9.vercel.app',
+  credentials: true
+}))
+
+app.use('/api/billing/webhook', express.raw({ type: 'application/json' }))
 app.use(express.json())
-app.use('/api/billing', billingRoutes);
+app.use('/api/billing', billingRoutes)
+
 app.use(session({
   secret: process.env.SESSION_SECRET || 'secret',
   resave: false,
